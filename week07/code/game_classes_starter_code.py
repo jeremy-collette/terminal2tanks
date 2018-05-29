197ce4a1baee6d627c60962ad5f7dd01abc9fd17
Jeremy Collette <jeremy.collette93@gmail.com>
2018-05-29 12:49:21 +1000

    Added week07 content

---
+++ week07/code/game_classes_starter_code.py
@@ -0,0 +1,60 @@
+# imports
+import pygame, sys
+from pygame.locals import *
+
+# setup constant variables
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 300
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Hello, pygame!')
+
+# handles pygame events
+def handle_pygame_events():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit() 
+
+# the Tank class represents a real-life tank!
+class Tank:
+    # constructor
+    def __init__(self):
+        self.color = (255, 255, 255)
+        self.pos = (175, 125)
+        self.radius = 50
+        self.speed = 2
+
+    # handles keyboard input
+    def handle_input(self):
+        # NOTE: copy handle_keyboard_input code here 
+
+    # handles screen wrapping
+    def handle_wrapping(self):
+        # NOTE: copy handle_wrapping code here
+
+    # updates the Tank
+    def update(self):
+        self.handle_input()
+        self.handle_wrapping()
+
+    # draws the Tank
+    def draw(self):
+        # NOTE: copy drawing code here
+
+
+# create Tank
+t = Tank()
+
+# main game loop
+while True:
+    # handle pygame events
+    handle_pygame_events()
+
+    # update Tank
+    t.update()
+
+    # draw Tank
+    t.draw()
